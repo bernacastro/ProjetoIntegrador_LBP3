4c98bcab2943f5e69652f9d874b2b22fe721147c
bernacastro <129337617+bernacastro@users.noreply.github.com>
2026-07-30 22:18:58 -0300

    Continuação: início da criação dos models

--- BibliotecaIgao/core/models.py
+++ BibliotecaIgao/core/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Livro(models.Model):
+    titulo = models.CharField(max_length=150)
+    isbn = models.IntegerField(max_length=13)
+    autor = models.ForeignKey('Autor', default="Desconhecido", on_delete=models.SET_DEFAULT)
+    data_publicacao = models.DateField()
+    quantidade = models.IntegerField(default=1)
+
+
+    def __str__(self):
+        return self.titulo
+    
+class Autor(models.Model):
+    nome = models.CharField(150)
+    data_nascimento = models.DateField()
+    nacionalidade = models.CharField()
--- BibliotecaIgao/core/urls.py
+++ BibliotecaIgao/core/urls.py
@@ -4,4 +4,5 @@ from .views import inicio, sobre
 urlpatterns = [
     path('', inicio, name='inicio'),
     path('sobre/', sobre, name='sobre'),
+    
 ]
